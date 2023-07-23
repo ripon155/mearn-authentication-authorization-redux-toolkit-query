f233e4173e451a12e2d06212be342680c2833c84
ripon155 <mohiuddin.saifullah@gmail.com>
2023-07-23 13:53:41 +0600

    adding user route and controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,35 @@
 import asyncHandler from 'express-async-handler';
 
+//api/users/auth
+//post
 export const authUser = asyncHandler(async (req, res) => {
-  res.status(401);
-  throw new Error('something went wrong');
   res.status(200).json({ message: 'Auth user' });
+});
+
+//@ Register User
+//api/users/register
+//post
+export const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Register user' });
+});
+
+//@ Logout  User
+//api/users/logout
+//get
+export const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Logout  user' });
+});
+
+//@ Get User   Profile
+//api/users/profile
+//get
+export const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Get User Profile user' });
+});
+
+//@ Update user profile
+//api/users/profile
+//patch
+export const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'update user profile ' });
 });
