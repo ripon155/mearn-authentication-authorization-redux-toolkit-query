cc0cec69529a0b39fabfe9515c6a68bd18c970ce
ripon155 <mohiuddin.saifullah@gmail.com>
2023-07-31 00:25:13 +0600

    protect route

--- frontend/src/store/slices/authSlice.js
+++ frontend/src/store/slices/authSlice.js
@@ -13,7 +13,6 @@ const authSlice = createSlice({
       localStorage.setItem('userInfo', JSON.stringify(action.payload));
     },
     logout: (state) => {
-      console.log('test');
       state.userInfo = null;
       localStorage.removeItem('userInfo');
     }
